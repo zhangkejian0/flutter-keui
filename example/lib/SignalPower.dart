f3364ea4dec0885f727682c916528224a90d6437
zhangkj@rayeye.cn <zhangkj@rayeye.cn>
2020-05-26 18:03:07 +0800

    电池and信号量

--- example/lib/SignalPower.dart
+++ example/lib/SignalPower.dart
@@ -1,2 +1,35 @@
 import 'package:flutter/material.dart';
 
+class SingalPower extends StatefulWidget {
+  @override
+  _SingalPowerState createState() => _SingalPowerState();
+}
+
+class _SingalPowerState extends State<SingalPower> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('电池and信号')
+      ),
+      body: Center(
+        
+      ),
+    );
+  }
+}
+
+// canvar
+
+class MyPainter extends CustomPainter{
+  
+  MyPainter();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
